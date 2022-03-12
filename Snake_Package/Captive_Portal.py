96e337760439e7ff663a82ebe4d14ca546634876
jac11 <rootx1982@gmail.com>
2022-03-12 14:19:37 -0500

    Add files via upload

--- Snake_Package/Captive_Portal.py
+++ Snake_Package/Captive_Portal.py
@@ -9,6 +9,8 @@ Cap_port_Path = str("/".join(os.path.dirname(__file__).split('/')[:-1]))+"/Capti
 Root_Web      = "        DocumentRoot /var/www/html"
 New_Root_Web  = "        #DocumentRoot /var/www/html"+'\n'+"        DocumentRoot "+ Cap_port_Path
 Header = "#Snake_web_Portal"+'\n'+' <Directory "'+Cap_port_Path+'>'
+Header2 = '#<Directory /var/www/html>'+'\n'+'<Directory "'+Cap_port_Path+'>'
+line_replace = '<Directory /var/www/html>'
 class Captive_Portal:
       
       def __init__(self):
@@ -33,8 +35,20 @@ class Captive_Portal:
                                   read_cont = rpacherewrite.read()
                         with open ("/etc/apache2/sites-enabled/000-default.conf",'w') as config_server :
                                    wireapacche = config_server.write( read_out+Header+read_cont)
+                        with open("etc/apache2/apache2.conf",'r') Config_FIE :
+                                 Read_Config_FILE = Config_FIE.readlines()
+                                 for line in Read_Config_FILE :                               
+                                     line = line.replace(line_replace,Header2)
+                                     with open ("etc/apache2/apache2conf.txt",'a') as write_Config:
+                                         write_output_F2 = write_Config.write(line) 
+                        with open ("etc/apache2/apache2conf.txt",'r') as Reread_Config: 
+                                  Read_Info = Reread_Config.read()
+                        with open("etc/apache2/apache2.conf",'w') write_Config_FIE :
+                                 Copy_Info = write_Config_FIE.write(Read_Info +'\n'+'ServerName  127.0.0.1')
+                        os.system('sudo systemctl reload httpd.service >/dev/null 2>&1')
                         os.system("systemctl restart apache2 >/dev/null 2>&1")
                         os.remove("/etc/apache2/sites-enabled/000-default.txt")
+                        os.remove("/etc/apache2/apache2conf.txt")
                         print("[+] Captive Portal Server is Up...") 
                     else:
                         os.system("systemctl restart apache2 >/dev/null 2>&1")
