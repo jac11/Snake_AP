e6e019608a78abd30e533ad3e8602a2d934f5e27
jac11 <rootx1982@gmail.com>
2022-03-16 15:26:44 -0400

    Add files via upload

--- Snake_Package/Host_apd.py
+++ Snake_Package/Host_apd.py
@@ -4,18 +4,8 @@ import os
 import time
 from subprocess import PIPE
 import subprocess
-Banner = """
-        __
-       {0O}
-       \__/
-       /^/  SNAKE ACCESS POINT START
-      / /____  TO exit  CTRL+C   
-     (_______) BY : JACSTROY
-   _(_________)_ 
-  (_____________)0o    
-          _     
-         (_)  
-"""
+from banner import *
+
 print (Banner)
 
 try:
---
+++ Snake_Package/banner.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+Banner = """
+        __
+       {0O}
+       \__/
+       /^/  SNAKE ACCESS POINT START
+      / /____  TO exit  CTRL+C   
+     (_______) BY : JACSTROY
+   _(_________)_ 
+  (_____________)0o    
+          _     
+         (_)  
+"""
+Banner2 = """
+      _    _
+   ,-(|)--(|)-.
+   \_   ..   _/
+     \______/
+       V  V   SNAKE_AP 
+       `.^^`. BY:JACSTORY                     
+         \^^^\                            
+         |^^^|                  
+   """
+
+
+
+
+
+
